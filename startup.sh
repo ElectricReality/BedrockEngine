eed3382c4948bc30f26b295f003333d4a39e7854
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2020-03-15 21:27:37 +0800

    Bedrock

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ${WORLD}
-sed -i -e "s/=19132/=${WORLD}/g" "${MCSERVERFOLDER}/server.properties"
+sed -i -e "s/=SERVERNAME/=${WORLD}/g" "${MCSERVERFOLDER}/server.properties"
 mkdir -p -- "${MCSERVERFOLDER}/worlds/default"
 cd /${MCSERVERFOLDER}/
 LD_LIBRARY_PATH=. ./bedrock_server
